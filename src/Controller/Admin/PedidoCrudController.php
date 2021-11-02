d819d9f9a1eb11d37869afe2bbf82a12d4c9d970
enucilli <emanuel.nucilli@gmail.com>
2021-11-02 17:44:50 -0300

    Actualizacion 2 noviembre

--- src/Controller/Admin/PedidoCrudController.php
+++ src/Controller/Admin/PedidoCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Pedido;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Asset;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use App\Entity\Cliente;
@@ -47,50 +48,67 @@ class PedidoCrudController extends AbstractCrudController
 
     public function configureCrud(Crud $crud): Crud
     {
-       return $crud
-        ->setPageTitle('index', 'Listado %entity_label_plural%');
+        return $crud
+
+
+            // defines the initial sorting applied to the list of entities
+            // (user can later change this sorting by clicking on the table columns)
+            ->setDefaultSort(['fechaEntrega' => 'DESC'])
+
+            // the max number of entities to display per page
+            ->setPaginatorPageSize(30)
+
+            ;
     }
 
     public function configureAssets(Assets $assets): Assets
     {
+
         return $assets
 
-            ->addJsFile('assets/crud/pedido.js');
+            //->addJsFile('assets/crud/campoPedido.js');
+            ->addJsFile(Asset::new('assets/crud/campoPedido.js')->defer());
     }
     
     public function configureFields(string $pageName): iterable
     {
-     
-        //$cliente=AssociationField::new('cliente')->setTemplatePath('custom_fields/cliente.html.twig');
-        $cliente=AssociationField::new('cliente')->autocomplete();
-        $productos=AssociationField::new('productos')->setTemplatePath('custom_fields/productos.html.twig');
-        $observaciones = TextEditorField::new('observaciones');
-        $precioTotal =  NumberField::new('precioTotal')->setFormTypeOption('disabled','disabled');
-        $fechaEntrega = DateTimeField::new('fechaEntrega');
-        $fechaRealizacion = DateTimeField::new('fechaRealizacion')->setSortable(true)->setFormTypeOption('disabled','disabled');
-        $direccionEntrega = TextField::new('direccionEntrega');
-        $retira= BooleanField::new('retiraPorLocal');
 
-        switch ($pageName){
-            case Crud::PAGE_INDEX:{
-                return [$cliente,$productos,$observaciones, $precioTotal,$fechaEntrega,$fechaRealizacion,$retira];
+        //$cliente=AssociationField::new('cliente')->setTemplatePath('custom_fields/cliente.html.twig');
+        $cliente = AssociationField::new('cliente')->autocomplete();
+        //$productos=AssociationField::new('productos')->setTemplatePath('custom_fields/productos.html.twig');
+        $productos = AssociationField::new('productos')->setCssClass('productoClass')->setTemplatePath('custom_fields/productos.html.twig');
+        $observaciones = TextEditorField::new('observaciones');
+        $precioTotal = NumberField::new('precioTotal')->setFormTypeOption('disabled', 'disabled');
+        $fechaEntrega = DateTimeField::new('fechaEntrega');
+        $fechaRealizacion = DateTimeField::new('fechaRealizacion')->setSortable(true)->setFormTypeOption('disabled', 'disabled');
+        $direccionEntrega = TextField::new('direccionEntrega');
+        $retira = BooleanField::new('retiraPorLocal');
+        $estadoPedido = AssociationField::new('estadoPedido');
+
+        switch ($pageName) {
+            case Crud::PAGE_INDEX:
+            {
+                return [$cliente, $productos, $precioTotal, $estadoPedido, $observaciones, $fechaEntrega, $fechaRealizacion, $retira];
                 break;
             }
-            case Crud::PAGE_NEW:{
-                return [$cliente,$productos,$precioTotal,$observaciones, $fechaEntrega,$fechaRealizacion,$retira];
+            case Crud::PAGE_NEW:
+            {
+                return [$cliente, $productos, $precioTotal, $observaciones, $fechaEntrega, $fechaRealizacion, $retira];
                 break;
             }
-            case Crud::PAGE_EDIT:{
-                return [$cliente,$productos,$observaciones, $precioTotal,$fechaEntrega,$fechaRealizacion,$retira];
+            case Crud::PAGE_EDIT:
+            {
+                return [$cliente, $productos, $precioTotal, $estadoPedido, $observaciones, $fechaEntrega, $fechaRealizacion, $retira];
                 break;
             }
-            case Crud::PAGE_DETAIL:{
-                return [$cliente,$productos,$observaciones, $precioTotal,$fechaEntrega,$fechaRealizacion,$retira];
+            case Crud::PAGE_DETAIL:
+            {
+                return [$cliente, $productos, $observaciones, $precioTotal, $fechaEntrega, $fechaRealizacion, $retira];
                 break;
             }
         }
 
 
+    }
     
-    
-}}
+}
